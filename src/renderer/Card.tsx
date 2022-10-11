754a49a968987c33a044aff54150f9dab423a083
Alex Ciarlillo <alex.ciarlillo@gmail.com>
2022-10-10 23:38:08 -0400

    add card move placeholder

--- src/renderer/Card.tsx
+++ src/renderer/Card.tsx
@@ -3,9 +3,7 @@ import Draggable from 'react-draggable';
 import classNames from 'classnames';
 import { useState, useEffect } from 'react';
 
-const Card = ({ scryfallId, draggable, tappable }) => {
-  const [tapped, setTapped] = useState(false);
-  const [moved, setMoved] = useState(false);
+const CardImg = ({ scryfallId }) => {
   const [imageUri, setImageUri] = useState(null);
 
   useEffect(() => {
@@ -15,6 +13,17 @@ const Card = ({ scryfallId, draggable, tappable }) => {
       )}/${scryfallId.charAt(1)}/${scryfallId}.jpg`
     );
   }, [scryfallId]);
+
+  return imageUri ? (
+    <img className="rounded-lg" src={imageUri} draggable={false} alt="" />
+  ) : null;
+};
+
+const Card = ({ scryfallId, draggable, tappable }) => {
+  const [tapped, setTapped] = useState(false);
+  const [moved, setMoved] = useState(false);
+
+  const [lastPosition, setLastPosition] = useState(null);
 
   toggleTapped = () => {
     if (!tappable) {
@@ -28,39 +37,68 @@ const Card = ({ scryfallId, draggable, tappable }) => {
     setMoved(false);
   };
 
+  onDragStart = (event, data) => {
+    setLastPosition({ x: data.lastX, y: data.lastY });
+  };
+
+  onDragStop = (...args) => {
+    setLastPosition(null);
+  };
+
   return (
-    <Draggable
-      onDrag={(args, data) => {
-        setMoved(true);
-      }}
-      disabled={!draggable}
-      handle=".handle"
-    >
-      <div className={classNames('absolute')}>
+    <div className="relative">
+      <Draggable
+        onDrag={(args, data) => {
+          setMoved(true);
+        }}
+        onStart={onDragStart}
+        onStop={onDragStop}
+        disabled={!draggable}
+        handle=".handle"
+      >
+        <div className={classNames('absolute', 'z-10')}>
+          <div
+            className={classNames(
+              'origin-center',
+              'flex',
+              'aspect-card',
+              'w-52',
+              'handle',
+              {
+                'rotate-90': tapped,
+              }
+            )}
+            onClick={toggleTapped}
+          >
+            <CardImg scryfallId={scryfallId} />
+          </div>
+        </div>
+      </Draggable>
+      {lastPosition && (
         <div
           className={classNames(
+            'absolute',
             'origin-center',
             'flex',
             'aspect-card',
             'w-52',
-            'handle',
-            {
-              'rotate-90': tapped,
-            }
+            'z-0',
+            'opacity-50'
           )}
-          onClick={toggleTapped}
+          style={{
+            transform: `translate(${lastPosition.x}px, ${lastPosition.y}px)`,
+          }}
         >
-          {imageUri ? (
-            <img
-              className="rounded-lg"
-              src={imageUri}
-              draggable={false}
-              alt=""
-            />
-          ) : null}
+          <div
+            className={classNames({
+              'rotate-90': tapped,
+            })}
+          >
+            <CardImg scryfallId={scryfallId} />
+          </div>
         </div>
-      </div>
-    </Draggable>
+      )}
+    </div>
   );
 };
 
